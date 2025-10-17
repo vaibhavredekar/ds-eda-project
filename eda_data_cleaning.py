f494db42fcf685236fc18117ea05a6a0cf899618
vaibhavredekar <vredekar84@gmail.com>
2025-10-17 09:15:31 +0200

    [Add]: Simple data visualization before and after

--- eda_data_cleaning.py
+++ eda_data_cleaning.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from time import sleep
 import missingno as msno
-
+import matplotlib.pyplot as plt
 
 
 class DataCleaning:
@@ -136,8 +136,12 @@ class DataCleaning:
         DataCleaning.df = updated_df.copy()
         print(DataCleaning.df.head())
         
-
-    def clean_data_considering_dtyp(self):
+    def clean_data_and_transformation(self):
+        '''
+        Data cleaning for complete table 
+        Saves the updated df to the class df and
+        return df
+        '''
         df_housing_prices_data = self.re_read_df_housing_price(self.dataset_file,',')
         new = self.dfcoln_chg_dtyp_operation(df_housing_prices_data, ['bedrooms','bathrooms','view','sqft_above','sqft_basement','sqft_living15','sqft_living','sqft_lot','sqft_lot15','waterfront','price','floors'], int)
         new1 = self.remove_str_char(new,'yr_renovated')
@@ -146,15 +150,25 @@ class DataCleaning:
         # Final df
         print(last.info(),'\n',last.head())
         self.save_df(last)
-
+        return last
 
     def main(self):
-        self.clean_data_considering_dtyp()
+        '''
+        Include flows for checking the data
+        '''
+        new_df = self.clean_data_and_transformation()
         print("Cleaned Data:\n","*"*30)
         print(DataCleaning.df.head())
         print("*"*30)
-        
 
+        # msno.bar(new_df,color='orange')
+        # plt.show()
+        df_housing_prices_data = self.re_read_df_housing_price(self.dataset_file,',')
+        msno.matrix(df_housing_prices_data, sparkline=True, figsize=(10,5), fontsize=12, color=(0.5, 0.58, 0.2))
+        plt.show()
+
+        msno.matrix(new_df, sparkline=True, figsize=(10,5), fontsize=12, color=(0.5, 0.58, 0.2))
+        plt.show()
 
 if __name__ == "__main__":
     DataCleaning("data\eda_house_price_details.csv").main()
